d510a4915e39643b62d9731e459c56f0462cd82b
yash782k <42696325+yash782k@users.noreply.github.com>
2024-11-05 21:16:35 +0530

    new sidebar

--- src/components/Auth/PrivateRoute.js
+++ src/components/Auth/PrivateRoute.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../../firebaseConfig'; // Adjust the path to your Firebase config
 
 const PrivateRoute = ({ children }) => {
-  return auth.currentUser ? children : <Navigate to="/" />;
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+      setLoading(false);
+    });
+    // Cleanup the listener on component unmount
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Replace with your preferred loading component
+  }
+
+  return isAuthenticated ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
