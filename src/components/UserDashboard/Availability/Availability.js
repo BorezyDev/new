2dfe22a78e8d81fd08374fb7d9bcd7b65440d40a
yash782k <42696325+yash782k@users.noreply.github.com>
2025-01-22 09:12:57 +0530

    vaibhav

--- src/components/UserDashboard/Availability/Availability.js
+++ src/components/UserDashboard/Availability/Availability.js
@@ -424,7 +424,7 @@ const BookingDashboard = () => {
 
           // Update the booking stage in Firestore
           await updateDoc(bookingDocRef, { 'userDetails.stage': newStage });
-          toast.log('Stage updated successfully for product:', productCode);
+          toast.success('Stage updated successfully ');
         }
       }
 
@@ -437,7 +437,7 @@ const BookingDashboard = () => {
         )
       );
     } catch (error) {
-      toast.error('Error updating booking stage:', error);
+      console.error('Error updating booking stage:', error);
     }
   };
 
@@ -490,10 +490,7 @@ const BookingDashboard = () => {
       day: '2-digit', 
       month: '2-digit', 
       year: 'numeric', 
-      hour: '2-digit', 
-      minute: '2-digit',
-      second:'2-digit',
-      hour12: true
+    
     }).format(date);
   };
   
